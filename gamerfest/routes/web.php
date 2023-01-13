e63daedf9bced5f233279c4716d3d9ace750daf6
Jcbarahon <jcbarahona3@espe.edu.ec>
2023-01-12 23:35:19 -0500

    Reportes version 3

--- gamerfest/routes/web.php
+++ gamerfest/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\auth;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 Route::get('/', function () {
     return view('Welcome');
 });
